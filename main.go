eaf92d99fb63409b9ee088944c2e6482b07b7e09
Yerzhan <yerzhan1993@gmail.com>
2023-03-01 13:49:22 +0600

    Refactoring records map
    Now the records map is in the customServer structure. Also, mutex is
    added to avoid race conditions. Besides that, the hostname IP is stored for a
    day.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -13,28 +15,78 @@ var (
 	ErrNotImplemented = fmt.Errorf("not implemented")
 )
 
-type customServer struct{}
+const (
+	// Caching for a day
+	timeLimit = 24 * time.Hour
+)
 
-var records = map[string]string{}
+type record struct {
+	ip   string
+	time time.Time
+}
 
-func parseQuery(m *dns.Msg) error {
+type customServer struct {
+	sync.RWMutex
+	records map[string]record
+}
+
+func (c *customServer) readRecord(hostname string) (string, bool) {
+	c.RLock()
+	record, ok := c.records[hostname]
+	c.RUnlock()
+	if !ok {
+		return "", false
+	}
+
+	if time.Since(record.time) >= timeLimit {
+		c.Lock()
+		delete(c.records, hostname)
+		c.Unlock()
+
+		return "", false
+	}
+
+	return record.ip, true
+}
+
+func (c *customServer) writeRecord(hostname, ip string) {
+	c.RLock()
+	_, ok := c.records[hostname]
+	c.RUnlock()
+	if ok {
+		log.Printf("%s is already cached", hostname)
+		return
+	}
+
+	c.Lock()
+	defer c.Unlock()
+	log.Printf("%s is cached", hostname)
+	c.records[hostname] = record{
+		ip:   ip,
+		time: time.Now(),
+	}
+}
+
+func (c *customServer) parseQuery(m *dns.Msg) error {
 	for _, q := range m.Question {
 		switch q.Qtype {
 		case dns.TypeA:
 			log.Printf("Query for %s\n", q.Name)
-			ip, ok := records[q.Name]
+			ip, ok := c.readRecord(q.Name)
 			if !ok {
 				log.Printf("%s is not cached", q.Name)
 				var err error
+
 				ip, err = lookupIP(q.Name)
 				if err != nil {
 					return err
 				}
-				records[q.Name] = ip
-				log.Printf("%s is added to cache", q.Name)
+
+				c.writeRecord(q.Name, ip)
 			} else {
 				log.Printf("%s is cached", q.Name)
 			}
+
 			rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
 			if err == nil {
 				m.Answer = append(m.Answer, rr)
@@ -73,7 +125,7 @@ func (c *customServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 
 	switch r.Opcode {
 	case dns.OpcodeQuery:
-		err := parseQuery(m)
+		err := c.parseQuery(m)
 		if err != nil && errors.Is(err, ErrNotImplemented) {
 			m.SetRcode(r, dns.RcodeNotImplemented)
 		} else if err != nil {
@@ -87,7 +139,9 @@ func (c *customServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 }
 
 func main() {
-	var c customServer
+	c := customServer{
+		records: make(map[string]record),
+	}
 	// start server
 	port := 1773
 	server := &dns.Server{
