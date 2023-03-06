d1e445fa17eb44e1bca1c44d115bcb7cfb7fcdbd
Yerzhan <yerzhan1993@gmail.com>
2023-03-06 13:05:43 +0600

    Added simple config package
    It stores port for http server, dns server, and secret key

---
+++ pkg/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+//nolint:tagliatelle
+type Config struct {
+	DnsPort   string `yaml:"dns-port"`
+	HttpPort  string `yaml:"http-port"`
+	SecretKey string `yaml:"secret-key"`
+}
+
+func NewConfig(path string) (Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+
+	c := new(Config)
+	err = yaml.Unmarshal(data, c)
+
+	return *c, err
+}
